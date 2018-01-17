65aaf92688b28e08bf33ffcb7f3a8eee194ce73e
Thaissa Falbo <thaissa.falbo@gmail.com>
2018-01-17 15:38:42 -0200

    Adds user model

--- models.py
+++ models.py
@@ -6,6 +6,18 @@ from passlib.apps import custom_app_context as pwd_context
 Base = declarative_base()
 
 #ADD YOUR USER MODEL HERE
+class User(Base):
+	__tablename__ = 'user'
+	id = Column(Integer, primary_key=True)
+	username = Column(String(32), index=True)
+	password_hash = Column(String(64))
+
+	def hash_password(self, password):
+		self.password_hash = pwd_context.encrypt(password)
+
+	def verify_password(self, password):
+		return pwd_context.verify(password, self.password_hash)
+
 
 class Bagel(Base):
 	__tablename__ = 'bagel'
@@ -26,7 +38,6 @@ class Bagel(Base):
 
 
 engine = create_engine('sqlite:///bagelShop.db')
- 
+
 
 Base.metadata.create_all(engine)
-    
